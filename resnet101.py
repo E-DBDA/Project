8fac7f70d814a4b8c8038dcdd7f4cbf05ea751c2
vishakha1598 <64003365+vishakha1598@users.noreply.github.com>
2021-02-26 22:35:03 +0530

    Add files via upload

---
+++ resnet101.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 25 21:52:48 2021
+
+@author: Vishakha
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 23 10:33:02 2021
+
+@author: Vishakha
+"""
+
+"""import splitfolders
+input_dataset="dataset"
+output_dataset="output"
+splitfolders.ratio(input_dataset,output_dataset,seed=42,ratio=(.6,.2,.2))"""
+
+from tensorflow.keras.layers import Conv2D,Flatten,Dense,MaxPool2D,BatchNormalization,GlobalAveragePooling2D
+from tensorflow.keras.applications.resnet50 import preprocess_input,decode_predictions
+from tensorflow.keras.preprocessing.image import ImageDataGenerator,load_img
+from tensorflow.keras.applications.resnet import ResNet101
+from tensorflow.keras.models import Model
+from tensorflow.keras import layers
+import matplotlib.pyplot as plt
+import numpy as np
+
+import tensorflow as tf
+import pandas as pd 
+import seaborn as sns
+
+RESNET50_POOLING_AVERAGE = 'avg'
+img_height,img_width = (224,224)
+batch_size = 32
+
+train_data_dir="output/train"
+test_data_dir="output/test"
+valid_data_dir="output/val"
+
+train_datagen = ImageDataGenerator(preprocessing_function=preprocess_input,shear_range=0.2,zoom_range=0.2,horizontal_flip=True)
+
+train_generator = train_datagen.flow_from_directory(train_data_dir,target_size=(img_height,img_width),batch_size=32,class_mode='categorical')
+valid_generator = train_datagen.flow_from_directory(valid_data_dir,target_size=(img_height,img_width),batch_size=32,class_mode='categorical')
+test_generator = train_datagen.flow_from_directory(test_data_dir,target_size=(img_height,img_width),batch_size=1,class_mode='categorical')
+
+#x,y = test_generator.next()
+#print(x.shape)
+
+base_model = tf.keras.applications.ResNet101(
+    include_top=False,
+    weights="imagenet",
+    classes=1000,
+    
+)
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+x = Dense(1024, activation='relu')(x)
+predictions = Dense(train_generator.num_classes,activation='softmax')(x)
+model = Model(inputs=base_model.input, outputs=predictions)
+
+for layer in base_model.layers:
+    layer.trainable = False
+
+model.compile(optimizer='adam',loss='categorical_crossentropy',metrics = ['accuracy'])
+model.fit(train_generator,epochs=10)
+
+#model.save("resnet101.h5")
+test_loss, test_acc =model.evaluate(test_generator,verbose = 2)
+#print("Test accuracy: ",test_acc)
+
+#model = tf.keras.models.load_model("savedmodel\resnet50_plants.h5")
+filenames = test_generator.filenames
+nb_samples = len(test_generator)
+y_prob = []
+y_act = []
+test_generator.reset()
+for _ in range(nb_samples):
+    X_test,Y_test = test_generator.next()
+    y_prob.append(model.predict(X_test))
+    y_act.append(Y_test)
+    
+predict_class = [list(train_generator.class_indices.keys())[i.argmax()] for i in y_prob]
+actcual_class = [list(train_generator.class_indices.keys())[i.argmax()] for i in y_act]
+
+out_df =pd.DataFrame(np.vstack([predict_class,actcual_class]).T,columns=['predict_class','actcual_class'])
+conf_matrix = pd.crosstab(out_df['actcual_class'],out_df["predict_class"], rownames=['actual'], colnames=['predicted'])
+
+sns.heatmap(conf_matrix,cmap='Blues',annot=True,fmt='d')
+plt.show()
+
+print("Test accuracy : {}".format((np.diagonal(conf_matrix).sum()/conf_matrix.sum().sum()*100)))
+import numpy as np
+from keras.preprocessing import image
+test_image = image.load_img('dataset/Artocarpus Heterophyllus (Jackfruit)/AH-S-016.jpg', target_size = (224, 224))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+ 
+# Predicting the final class
+result = model.predict(test_image)[0].argmax()
+ 
+# Fetching the class labels
+labels = train_generator.class_indices
+labels = list(labels.items())
+ 
+# Printing the final label
+for label, i in labels:
+    if i == result:
+        print("The test image has: ", label)
+        break
